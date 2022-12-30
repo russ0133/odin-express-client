62f55c142ff74da2128ebe256f6330bf4e864a02
Gabriel <gabrieldsfs@gmail.com>
2022-12-30 20:53:32 +0400

    add remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const newPerson = async () => {
-    const response = await axios.post("http://localhost:3000/api/person", {
-      firstName: "John",
-      lastName: "Smith",
-    });
-    console.log(response.data);
-  };
+  const [persons, setPersons] = useState([]);
 
   const getPersons = async () => {
-    const response = await axios.get("http://localhost:3000/api/person");
+    axios.get("http://localhost:3000/api/person").then((response) => {
+      setPersons(response.data);
+    });
+  };
+  useEffect(() => {
+    getPersons();
+  }, []);
+
+  const newPerson = async () => {
+    const firstName = window.prompt("Enter a first name");
+    const lastName = window.prompt("Enter a last name");
+    const response = await axios.post("http://localhost:3000/api/person", {
+      firstName: firstName,
+      lastName: lastName,
+    });
     console.log(response.data);
+    getPersons();
+  };
+
+  const deletePerson = async (id: string) => {
+    const response = await axios.delete(`http://localhost:3000/api/person/${id}`);
+    console.log(response.data);
+    getPersons();
   };
 
   return (
@@ -29,15 +42,22 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>CRUD for Odin Project</h1>
       <div className="card">
         <button onClick={() => newPerson()}>Send</button>
-        <button onClick={() => getPersons()}>Get</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button>Get</button>
+        <p>List of persons:</p>
+        {persons.map(({ id, firstName, lastName }) => {
+          return (
+            <div key={id}>
+              ({id}) {firstName} {lastName}
+              <span>
+                <button onClick={() => deletePerson(id)}>Delete</button>
+              </span>
+            </div>
+          );
+        })}
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
